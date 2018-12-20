766834702b6cfd34f64b693c5b83a444453850a9
NPerieff <43181482+NPerieff@users.noreply.github.com>
2018-12-20 10:15:50 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,88 @@
+$(".h1btn").click(function() {
+    $(".h1cntnt").show();
+    $(".h2cntnt").hide();
+    $(".pcntnt").hide();
+});
+$(".h2btn").click(function() {
+    $(".h1cntnt").hide();
+    $(".h2cntnt").show();
+    $(".pcntnt").hide();
+});
+$(".pbtn").click(function() {
+    $(".h1cntnt").hide();
+    $(".h2cntnt").hide();
+    $(".pcntnt").show();
+});
+$(".search").click(function() {
+    var search = $("input").val();
+//Paragraph
+    if (search==="paragraph") {
+        window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+    else if (search==="Paragraph" || search==="p" || search==="p tag" || search==="<p>") {
+        window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+    else {
+         window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+//Heading
+    if (search==="heading") {
+        window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+    else if (search==="<h1>" || search==="<h2>" || search==="<h3>" || search==="<h4>" || search==="<h5>" || search==="<h6>" || search==="Heading") {
+        window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+    else {
+         window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+//Ordered List
+    if (search==="ordered list") {
+        window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+    else if (search==="ordered List" || search==="Ordered list" || search==="ol" || search==="ol tag" || search==="<ol>") {
+        window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+    else {
+         window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+//Button
+    if (search==="button") {
+        window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+    else if (search==="Button" || search==="<button>" || search==="button tag") {
+        window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+    else {
+         window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+//Unordered List
+    if (search==="unordered list") {
+        window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+    else if (search==="unordered List" || search==="Unordered list" || search==="ul" || search==="ul tag" || search==="<ul>") {
+        window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+    else {
+         window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+//Div
+    if (search==="div") {
+        window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+    else if (search==="Div" || search==="div tag" || search==="<div>") {
+        window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+    else {
+         window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+//Input
+    if (search==="input") {
+        window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+    else if (search==="Input" || search==="<input>" || search==="input tag") {
+        window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+    else {
+         window.open("https://www.google.com/search?q="+search, "_blank");
+    }
+});
